9f561cf546e0bee7726b7284c5667ad0c44927e2
Max Arrich <max_x@live.at>
2019-06-08 15:40:46 +0200

    first exploration o fthe data

--- Project/lgd.R
+++ Project/lgd.R
@@ -4,21 +4,16 @@
 # Student IDs                   : 15605017  - 13614011            #
 # *************************************************************** #
 
-## Working Directory Setting
-wd <- dirname(rstudioapi::getSourceEditorContext()$path)
-setwd(wd)
-
 
 
 
 
 # ============================== Library Calls  ==============================
 
-toload <- c("data.table","lubridate","zoo","xts","readxl","readr","ggplot2","pander","tableHTML","stringr","gdata","MASS","copula",
-            "mvnmle","tseries","VineCopula","stargazer","psych","gtable","gridExtra","lmtest","scales","latex2exp","ggfortify","magrittr")
+toload <- c("magrittr","plyr","reshape2","neuralnet")
 toinstall <- toload[which(toload %in% installed.packages()[,1] == F)]
-lapply(toinstall, install.packages, character.only = TRUE)
-lapply(toload, require, character.only = TRUE)
+sapply(toinstall, install.packages, character.only = TRUE)
+sapply(toload, require, character.only = TRUE)
 
 
 
@@ -26,6 +21,83 @@ lapply(toload, require, character.only = TRUE)
 
 # ============================== Read Data  ==============================
 
+
+## Working Directory Setting
+wd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(wd)
+
+## Read Data from CSV
+dat <- read.csv("/Users/Max/Dropbox/Risikomanagement/Uni/ML/LGD/mortgage.csv") %T>% attach
+
+## Remove NA observations
+dat <- dat[complete.cases(dat),]
+
+
+
+
+# Generate Neural Network ------------------------------------------
+
+
+
+
+# Scale data for neural network, we do not scale id and time
+max <-  apply(dat[,-c(1,2)], 2 , max)
+min <-  apply(dat[,-c(1,2)], 2 , min)
+dat_scaled <-  cbind(dat[,c(1,2)], as.data.frame(scale(dat[,-c(1,2)], center = min, scale = max - min)))
+
+
+# Select some point in time to train network. We exclude id,time, repayment and the status variable since it is redundant
+train_data <- dat_scaled[which(dat[, 2] == 15), ][,-c(1,2,22,23)]
+
+test_data <- dat_scaled[which(dat[, 2] == 16), ][,-c(1,2,22,23)]
+  
+
+# Fit neural network
+nn <- neuralnet(default_time ~ ., data = train_data, hidden = c(10,10), act.fct = "logistic", 
+                err.fct = "sse", lifesign = "full", threshold = 0.05)
+
+# Get predictions for the testing set
+predicted = ifelse(predict(nn, test_data) > 0.5,1,0)
+
+
+
+
+
+## Logistic regression as benchmark
+
+log_reg <- glm(default_time ~ ., data = dat_scaled[,-c(1,2,22,23)], family = "binomial")
+
+pred <- ifelse(fitted(log_reg) > 0.5,1,0)
+
+
+correct <- table(pred == dat_scaled$default_time)
+
+
+
+
+
+
+
+length(pred)
+length(predicted)
+nrow(test_data)
+
+table(pred == predicted)
+
+
+correct <- sum(ifelse(predicted == test_data$default_time,1,0))
+
+false <- sum(ifelse(predicted != test_data$default_time,1,0))
+
+unique(predicted)
+
+false <- sum(test_data$default_time)
+
+
+nrow(predicted)
+length(train_data$default_time)
+
+mse <- sum((test_data$default_time - predicted)^2) / nrow(test_data)
 
 
 
