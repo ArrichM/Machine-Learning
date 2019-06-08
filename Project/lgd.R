21463c5a7d4e6a392c778d6b2b8d35a51bfd530e
Max Arrich <max_x@live.at>
2019-06-08 18:36:21 +0200

    evaluation of different layer / neuron combinations, random forest addd, lasso added

--- Project/lgd.R
+++ Project/lgd.R
@@ -10,7 +10,7 @@
 
 # ============================== Library Calls  ==============================
 
-toload <- c("magrittr","plyr","reshape2","neuralnet")
+toload <- c("magrittr","plyr","reshape2","neuralnet","randomForest","glmnet")
 toinstall <- toload[which(toload %in% installed.packages()[,1] == F)]
 sapply(toinstall, install.packages, character.only = TRUE)
 sapply(toload, require, character.only = TRUE)
@@ -54,7 +54,7 @@ scaled_data <-  cbind(dat[,c(1,2)], as.data.frame(scale(dat[,-c(1,2)], center = 
 set.seed(100)
 
 # Sample n data at random
-temp_data <- scaled_data[sample(1:nrow(scaled_data),50000),]
+temp_data <- scaled_data[sample(1:nrow(scaled_data),5000),]
 
 
 # Select 2/3 of the data as trainign data
@@ -75,31 +75,34 @@ test_data <- temp_data[-index, ][,-c(1:4,22,23)]
 
 # ============================== Generate Neural Network  ==============================
 
+fit_nn <- function(layers = c(1,1), train_data1 = train_data, test_data1 = test_data, tr = 0.05){
+  
+  # Fit neural network
+  nn <- neuralnet(default_time ~ ., data = train_data1, hidden = c(7,2), act.fct = "logistic", linear.output = F,
+                  err.fct = "sse", lifesign = "full", threshold = tr, algorithm = "sag", learningrate.factor = list( minus = 0.5, plus = 1.2))
+  
+  
+  # Get predictions for the testing set
+  predicted_nn = predict(nn, test_data1)
+  predicted_nn_bin <- ifelse(predicted_nn > 0.5, 1,0)
+  
+  # How many defualts were predicted?
+  table(predicted_nn_bin)
+  
+  # Compare with actual data, did we get something right?
+  compare <- cbind(predicted_nn_bin, test_data1$default_time)[which(test_data1$default_time == 1),] %T>% print
+  
+  # How many did we get wrong / right
+  table(predicted_nn_bin == test_data$default_time)
+  
+  
+  # Compute MSE from probabilities
+  mse_nn <- mean((predicted_nn - test_data$default_time) ^2) %T>% return
 
-# Fit neural network
-nn <- neuralnet(default_time ~ ., data = train_data, hidden = 10, act.fct = "logistic", linear.output = F,
-                err.fct = "sse", lifesign = "full", threshold = 0.01, algorithm = "sag", learningrate.factor = list( minus = 0.5, plus = 1.2))
+}
 
-
-# Get predictions for the testing set
-predicted_nn = predict(nn, test_data)
-predicted_nn_bin <- ifelse(predicted_nn > 0.5, 1,0)
-
-# How many defualts were predicted?
-table(predicted_nn_bin)
-
-# Compare with actual data, did we get something right?
-compare <- cbind(predicted_nn_bin, test_data$default_time)[which(test_data$default_time == 1),] %T>% print
-
-# How many did we get wrong / right
-table(predicted_nn_bin == test_data$default_time)
-
-
-# Compute MSE from probabilities
-mse_nn <- mean((predicted_nn - test_data$default_time) ^2)
-
-
-
+# Evaluate a number of possible layer / neuron combinations
+eval <- apply(expand.grid(4:10,3:6),1, fit_nn, tr = 0.02)
 
 
 
@@ -127,11 +130,30 @@ mse_log <- mean((predicted_reg - test_data$default_time) ^2)
 
 
 
+# ============================== Random Forest  ==============================
+
+# Fit Random Forest, we run the algorithm in classification mode
+random_forest <- randomForest(as.factor(default_time) ~ ., data = train_data)
+
+# Get predictions for the testing set
+predicted_rf <- predict(random_forest, data = test_data)
+
+
+random_forest$importance
 
 
 
 
 
+
+
+
+
+
+# ============================== LASSO Regression  ==============================
+
+lambda <- 10^seq(10, -2, length = 100)
+lasso.mod <- glmnet(default_time ~ ., data = train_data, alpha = 1, family = "binomial")
 
 
 
