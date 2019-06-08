8f4c78d027752f5afc54fd47b6ecff2a37a66f12
Max Arrich <max_x@live.at>
2019-06-08 12:25:47 +0200

    Code initialisation

--- Project/lgd.R
+++ Project/lgd.R
@@ -1,1 +1,43 @@
-###### Code file for the group project ######
+# *************************************************************** #
+# Machine Learning              : Group Project                   #
+# Code File                     : Main                            #
+# Student IDs                   : 15605017  - 13614011            #
+# *************************************************************** #
+
+## Working Directory Setting
+wd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(wd)
+
+
+
+
+
+# ============================== Library Calls  ==============================
+
+toload <- c("data.table","lubridate","zoo","xts","readxl","readr","ggplot2","pander","tableHTML","stringr","gdata","MASS","copula",
+            "mvnmle","tseries","VineCopula","stargazer","psych","gtable","gridExtra","lmtest","scales","latex2exp","ggfortify","magrittr")
+toinstall <- toload[which(toload %in% installed.packages()[,1] == F)]
+lapply(toinstall, install.packages, character.only = TRUE)
+lapply(toload, require, character.only = TRUE)
+
+
+
+
+
+# ============================== Read Data  ==============================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
