ef19dded2609b2e1b24bda5556406b6be7fd131f
Max Arrich <max_x@live.at>
2019-06-08 17:16:36 +0200

    cleanup

--- Project/lgd.R
+++ Project/lgd.R
@@ -35,9 +35,13 @@ dat <- dat[complete.cases(dat),]
 dat$age <- dat$time- dat$orig_time
 
 
-# Generate Neural Network ------------------------------------------
 
 
+
+
+
+
+# ============================== Prepare Data ==============================
 
 
 # Scale data for neural network, we do not scale id and time
@@ -49,14 +53,28 @@ scaled_data <-  cbind(dat[,c(1,2)], as.data.frame(scale(dat[,-c(1,2)], center = 
 # We select a subset of the data and split it into training and testing
 set.seed(100)
 
-temp_data <- scaled_data[sample(1:nrow(scaled_data),3000),]
+# Sample n data at random
+temp_data <- scaled_data[sample(1:nrow(scaled_data),10000),]
 
+
+# Select 2/3 of the data as trainign data
 index <- sample(1:nrow(temp_data), (nrow(temp_data)*0.6) %>% ceiling)
 
-train_data <- temp_data[index,][,-c(1,2,22,23)]
+train_data <- temp_data[index,][,-c(1:4,22,23)]
 
-test_data <- temp_data[-index, ][,-c(1,2,22,23)]
-  
+test_data <- temp_data[-index, ][,-c(1:4,22,23)]
+
+
+
+
+
+
+
+
+
+
+# ============================== Generate Neural Network  ==============================
+
 
 # Fit neural network
 nn <- neuralnet(default_time ~ ., data = train_data, hidden = 10, act.fct = "logistic", linear.output = F,
@@ -64,54 +82,42 @@ nn <- neuralnet(default_time ~ ., data = train_data, hidden = 10, act.fct = "log
 
 
 # Get predictions for the testing set
-predicted = ifelse(predict(nn, test_data) > 0.5,1,0)
-table(predicted)
+predicted_nn = ifelse(predict(nn, test_data) > 0.5,1,0)
+
+# How many defualts were predicted?
+table(predicted_nn)
 
 # Compare with actual data, did we get something right?
-compare <- cbind(predicted, test_data$default_time)[which(test_data$default_time == 1),] %T>% print
+compare <- cbind(predicted_nn, test_data$default_time)[which(test_data$default_time == 1),] %T>% print
 
-table(predicted == test_data$default_time)
-table(test_data$default_time)
+# How many did we get wrong 7 right
+table(predicted_nn == test_data$default_time)
 
 
-## Logistic regression as benchmark
+# Compute MSE
 
+
+
+
+
+
+
+
+# ============================== Logistic Regression  ==============================
+
+
+#We perform logistic regression as benchmark model
 log_reg <- glm(default_time ~ ., data = train_data, family = "binomial")
 
+# Get predictions for the testing set
+predicted_reg <- ifelse(predict(log_reg, newdata = test_data) > 0.5,1,0)
 
-pred <- ifelse(predict(log_reg, newdata = test_data) > 0.5,1,0)
+# How many defualts were predicted?
+table(predicted_reg) 
 
-cbind(pred, test_data$default_time)[which(test_data$default_time == 1),]
+# Compare with actual data, did we get something right?
+compare_reg <- cbind(predicted_reg, test_data$default_time)[which(test_data$default_time == 1),] %T>% print
 
-
-table(pred == test_data$default_time)
-
-plot(predict(nn, test_data),predict(log_reg, newdata = test_data) ) 
-
-min(predict(nn, test_data))
-
-
-
-length(pred)
-length(predicted)
-nrow(test_data)
-
-table(pred == predicted)
-
-
-correct <- sum(ifelse(predicted == test_data$default_time,1,0))
-
-false <- sum(ifelse(predicted != test_data$default_time,1,0))
-
-unique(predicted)
-
-false <- sum(test_data$default_time)
-
-
-nrow(predicted)
-length(train_data$default_time)
-
-mse <- sum((test_data$default_time - predicted)^2) / nrow(test_data)
 
 
 
