a672c0c5413b91c6c5ccd31f2a9277bf9be996a3
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2019-06-10 03:43:39 +0200

    Changed path, changed evaluate model function, added evaluation plot, added cross validations with all kinds of ML algos (Boosted Logit, boosted linear, Random forrest trained, ...)

--- Project/lgd.R
+++ Project/lgd.R
@@ -16,7 +16,7 @@
 
 # ============================== Library Calls  ==============================
 
-toload <- c("magrittr","plyr","reshape2","neuralnet","randomForest","glmnet")
+toload <- c("magrittr","plyr","reshape2","neuralnet","randomForest","glmnet", "caret", "rlist", "tidyr", "mboost")
 toinstall <- toload[which(toload %in% installed.packages()[,1] == F)]
 sapply(toinstall, install.packages, character.only = TRUE)
 sapply(toload, require, character.only = TRUE)
@@ -31,19 +31,25 @@ sapply(toload, require, character.only = TRUE)
 ## Working Directory Setting
 wd <- dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(wd)
-
+set.seed(100)
 ## Read Data from CSV
-dat <- read.csv("/Users/Max/Dropbox/Risikomanagement/Uni/ML/LGD/mortgage.csv") %T>% attach
-
+dat <- read.csv(paste0(wd, "/Data/mortgage.csv")) %T>% attach
 ## Remove NA observations
 dat <- dat[complete.cases(dat),]
 
 # Add age of position
 dat$age <- dat$time- dat$orig_time
 
+# For speed and convenience we often use a smaller subset of the whole data. Specify if you want to use the whole set or a subset
+dataset_type <- askYesNo("Do you want to use a subset of the data to increase speed instead of the whole set?")
 
+if(dataset_type==TRUE){
+  reduced_data <- createDataPartition(y=dat$default_time, p=1/220, list=FALSE) # randomly shrinks dataset to n= 3000 datapoints
+  dat          <- dat[ reduced_data,]
+} else {return(0)}
 
-
+n <- nrow(dat)
+n
 
 
 
@@ -68,7 +74,7 @@ fit_nn <- function(layers = c(1,1), train_data1 = train_data, test_data1 = test_
 }
 
 # Shuffle data and create training and testing set on the go
-shuffle <- function(n = 5000, data = scaled_data, ratio = 2/3, check = T){
+shuffle <- function(n = nrow(dat), data = scaled_data, ratio = 2/3, check = T){
   
 
   # Check function call
@@ -110,26 +116,62 @@ prediction_matrix <- function(predictions, observations = test_data$default_time
   
 }
 
-# Function to evaluate performance of models
-evaluate_model <- function(model, observations = test_data$default_time, best_lamb = best_lambda, tr = 0.5){
+# Function to evaluate performance of models. Always enter models in form of lists
+evaluate_model <- function(model=list(...), modelname, observations = test_data$default_time, best_lamb = best_lambda, tr = 0.5){ #modelname added
  
-  #get predictions from the model
-  if(grepl("lasso", deparse(substitute(model))) ){
-    predictions <- predict(model, newx = test_data[ ,-17] %>% as.matrix, s = best_lamb, type = "response")
+
+  
+  n = length(model)
+  tables    = list()
+  metrics   = list()
+  #transform to binary response
+  for (i in 1:n){
+    #get predictions from the model
+    if(grepl("lasso", deparse(substitute(model))) ){
+      predictions <- predict(model, newx = test_data[ ,-17] %>% as.matrix, s = best_lamb, type = "response")
+      
+    }else{  
+      predictions <- as.integer(predict(model[[i]], newdata=test_data))-1#, type = "response")
+    }
+    predictions_bin <- ifelse(predictions < tr, 0,1)
     
-  }else{  
-    predictions <- predict(model, test_data, type = "response")
+    table  <- prediction_matrix(predictions)
+    # table  <- table(observations, predictions_bin, dnn = c("Actual defaults", "Predicted defaults"))[c(2,1),c(2,1)] # Create matrix and Change order columns of matrix
+    
+    TRP    <- table[1,1]/(table[1,1]+table[1,2])   # True Positive Rate (TPR) or sensitivity or recall or hit rate is a measure of how
+    # many true positives were identified out of all the positives identified: Ideally, the model is better if we have this closer to one
+    TNR    <- table[2,2]/(table[2,2]+table[2,1])   # True Negative Rate (TNR) or specificity is the ratio of true negatives and total
+    # number of negatives we have predicted:
+    # If this ratio is closer to zero, the model is more accurate.
+    ACC    <- (table[1,1]+table[2,2])/(sum(table)) # Accuracy is the measure of how good our model is. It is expected to be closer to 1, if
+    # our model is performing well.
+    Prec   <- table[1,1]/(table[1,1]+table[2,1])   # Precision is defined as how many selected items are relevant. That is, how many of
+    # the predicted ones are actually correctly predicted. If precision is closer to one, we are more accurate in our predictions
+    Recall <- table[1,1]/(table[1,1]+table[1,2])   # Recall, on the other hand, tells how many relevant items we selected.
+    
+    mse_cont    <- (predictions-observations) ^2 %>% mean
+    mse_bin     <- (predictions-observations) ^2 %>% mean
+    metrics[[i]]<- data.frame(Model = modelname[i], TRP = TRP, TNR=TNR, ACC=ACC, Precision=Prec, Recall=Recall, MSE_cont = mse_cont, MSE_bin=mse_bin)
+    tables[[i]] <- table
   }
   
-  #transform to binary response
-  predictions_bin <- ifelse(predictions < tr, 0,1)
+  names(tables) <- modelname
+  metrics_all   <- do.call(rbind, metrics)
   
-  mat <- prediction_matrix(predictions)
-  detected_defaults <- mat[1,1]/colSums(mat)[1] %>% set_names("Ratio")
-  mse_cont <- (predictions-observations) ^2 %>% mean
-  mse_bin <- (predictions-observations) ^2 %>% mean
+  list(tables, metrics_all)
+  #return(list(matrix = mat, detected_defaults = detected_defaults, mse_cont = mse_cont, mse_bin = mse_bin))
+}
+
+
+# Feed with input from evaluate_model
+
+plot.evaluation <- function(evaluate_model_object){ # Insert valuation metrics
   
-  return(list(matrix = mat, detected_defaults = detected_defaults, mse_cont = mse_cont, mse_bin = mse_bin))
+  metrics_all <- tail(evaluate_model_object,1)
+  df <- gather(metrics_all[[1]][, 1:5], key="metric", value="value", -Model) # Bringing in correct shape to feed into ggplot
+  ggplot(data=df, aes(x=Model, y=value, color=Model, fill=Model))+ #https://www.r-graph-gallery.com/48-grouped-barplot-with-ggplot2/
+    geom_bar(stat="identity")+
+    facet_wrap(~metric)
 }
 
 
@@ -146,10 +188,9 @@ min <-  apply(dat[,-c(1,2)], 2 , min)
 scaled_data <-  cbind(dat[,c(1,2)], as.data.frame(scale(dat[,-c(1,2)], center = min, scale = max - min)))
 
 # Set seed ro reproducibility
-set.seed(99)
 
 # We select a subset of the data and split it into training and testing
-shuffle(n = 5000, check = "none")
+shuffle(n = n, check = "none")
 
 
 
@@ -167,14 +208,14 @@ eval <- apply(expand.grid(4:10,3:6),1, fit_nn, tr = 0.05)
 
 # Fit best network and save under name "nn". For the fitting, we draw a larger dataset
 set.seed(99)
-shuffle(n= 50000, check = "none")
+shuffle(n= n, check = "none")
 
 nn <- fit_nn(expand.grid(4:10,3:6)[which.min(eval),] %>% unlist, create_network = T, tr = 0.05)
 
 
 
 # Get predictions for the testing set
-predicted_nn <- predict(nn, test_data, type = "response") 
+evaluate_model(list(nn), modelname = c("Neural Network")) 
 
 
 
@@ -197,12 +238,34 @@ predicted_reg <- predict(log_reg, newdata = test_data, type = "response")
 
 
 # Compare logistic regression and neural network
-plot(predicted_nn, predicted_reg)
+#plot(predicted_nn, predicted_reg)
+evaluate_model(list(log_reg, nn), modelname = c( "Logistic Regression", "Neural Network"))
 
 
 
 
 
+
+# == Alternative: Boosted Logistic Regression ==
+
+# Here we use a very handy package to perform k-fold cross validation. The package requires its own training and testing sample splitting procedure
+intrain <- createDataPartition(y=dat$default_time, p=2/3, list=FALSE) # splits the data according to y argument and ensures that calls distribution of the data remains
+str(intrain) # Output is a set of integers for the rows of dat that belong to the training set
+train_data <- dat[ intrain,]
+test_data  <- dat[-intrain,]
+
+# initialize cross validation Folds. Here we use 5-fold cross validation, repeated 3 times
+fitControl <- trainControl(method="repeatedcv", number = 5, repeats = 3)  
+
+# Fit boosted logistic/probit regression
+logitboost_fit <- caret::train(as.factor(default_time) ~ ., data=train_data, method="LogitBoost", trControl = fitControl)
+
+# Looking at how accuracy increases over the training procedure
+trellis.par.set(caretTheme())
+plot(logitboost_fit) 
+
+# 
+evaluate_model(list(log_reg, nn, logitboost_fit), modelname = c("Logistic Regression", "Neural Network", "LogitBoost"))
 
 
 
@@ -221,15 +284,23 @@ predicted_rf <- predict(random_forest, data = test_data) %>% as.character %>% as
 prediction_matrix(predicted_rf)
 
 # Compare with actual data, did we get something right?
-defaults_captured(predicted_rf)
+# defaults_captured(predicted_rf)
 
 # Importance of variables
 random_forest$importance
+evaluate_model(list(log_reg, nn, logitboost_fit, random_forest), modelname = c("Logistic Regression", "Neural Network", "LogitBoost", "untrained RF"))
 
 
+#=============================== Alternative Way to get Random Forrest via cross validation =========================
 
+rf_fit     <- caret::train(as.factor(default_time) ~ ., data=train_data, method="ranger", trControl = fitControl) # We use "ranger" method for random forrest. The training algorithm seeks to optimize accuracy.
 
+# Looking at how accuracy increases over the training procedure
+trellis.par.set(caretTheme())
+plot(rf_fit) 
 
+# Lets look how it performs on the test sample.
+evaluate_model(list(log_reg, nn, logitboost_fit, random_forest, rf_fit), modelname = c("Logistic Regression", "Neural Network", "LogitBoost", "untrained RF", "trained RF"))
 
 
 
@@ -264,35 +335,45 @@ plot(predicted_reg, predicted_lasso)
 
 
 
+# ============================== glmboost  ==============================
+
+glm_fit     <- caret::train(as.factor(default_time) ~ ., data=train_data, method="glmboost", trControl = fitControl) # We use "ranger" method for random forrest. The training algorithm seeks to optimize accuracy.
+
+# Looking at how accuracy increases over the training procedure
+plot(glm_fit) 
+
+# Lets look how it performs on the test sample.
+metrics <- evaluate_model(list(log_reg, nn, logitboost_fit, random_forest, rf_fit, glm_fit), modelname = c("Logistic Regression", "Neural Network", "LogitBoost", "untrained RF", "trained RF", "GLM Boost"))
+metrics 
+plot.evaluation(metrics)
 
 
-
-
-
-# ============================== Comparison  ==============================
-
-# Compare the performance of the differnet methods. We evaluate at increasingly large datasets
-
-evaluate_model(nn)
-evaluate_model(log_reg)
-evaluate_model(log_lasso)
-
-
-# Reshuffle and draw larger testing data
-shuffle(n = 300000, ratio = 1/100)
-
-# In large samples, the NN performs much better
-evaluate_model(nn)
-evaluate_model(log_reg)
-evaluate_model(log_lasso)
-
-
-# Reshuffle and draw larger testing data
-shuffle(n = 500000, ratio = 1/100)
-
-# The fit seems to be stable, with a stable correct prediction rate
-evaluate_model(nn)
-evaluate_model(log_reg)
-evaluate_model(log_lasso)
-
+there_are_no_errors <- F
+if(there_are_no_errors){
+  # ============================== Comparison  ==============================
+  
+  # Compare the performance of the differnet methods. We evaluate at increasingly large datasets
+  
+  evaluate_model(nn, modelname = "nn")
+  evaluate_model(log_reg)
+  evaluate_model(log_lasso)
+  
+  
+  # Reshuffle and draw larger testing data
+  shuffle(n = 300000, ratio = 1/100)
+  
+  # In large samples, the NN performs much better
+  evaluate_model(nn)
+  evaluate_model(log_reg)
+  evaluate_model(log_lasso)
+  
+  
+  # Reshuffle and draw larger testing data
+  shuffle(n = 500000, ratio = 1/100)
+  
+  # The fit seems to be stable, with a stable correct prediction rate
+  evaluate_model(nn)
+  evaluate_model(log_reg)
+  evaluate_model(log_lasso)
+}
 
