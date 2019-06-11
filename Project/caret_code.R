a87c9e17fbbcab84d3d0f3c0eef07d8cac4dab36
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2019-06-11 16:14:54 +0200

    Changed shuffle output: For easier computation they are now levels, changed confusion matrix which was wrong. Changes apply to both files

--- Project/caret_code.R
+++ Project/caret_code.R
@@ -64,7 +64,7 @@ get_lags <- function(c_id = 1, data = dat, lag = 2, lookup = c(6,7,8,9,10,11)){
 }
 
 # Shuffle data and create training and testing set on the go. Also we do a number of manipulations to improve performance
-shuffle <- function(n = nrow(dat), data = dat, ratio = 2/3, lags = NULL, unwanted = c(1:2,22,23), col_to_lag = c(6,7,8,9,10,11), lasso_drop = NULL){
+shuffle <- function(n = nrow(dat), data = dat, ratio = 2/3, lags = NULL, unwanted = c(1:2,22,23), col_to_lag = c(6,7,8,9,10,11)){
   
   # Sample n data at random
   if(is.null(lags) == T) temp_data <- data[sample(1:nrow(data),n),]
@@ -80,15 +80,13 @@ shuffle <- function(n = nrow(dat), data = dat, ratio = 2/3, lags = NULL, unwante
     
     #remove NA
     temp_data <- temp_data[complete.cases(temp_data),] %>% set_colnames( c(colnames(dat),
-                                                                           paste0(rep(colnames(dat)[col_to_lag], each = lags),"_L",rep(1:lags,length(col_to_lag))) ) )
+                                                                           paste0(rep(colnames(dat)[col_to_lag], each = lags),"-L",rep(1:lags,length(col_to_lag))) ) )
     
     
   }
   
   # Remove unwanted columns
   temp_data <- temp_data[,-unwanted]
-  if(is.null(lasso_drop) == F)  temp_data <- temp_data[,-lasso_drop]
-
   
   # Scale data for neural network, we do not scale id and time
   min <- apply(temp_data, 2 , min)
@@ -96,8 +94,17 @@ shuffle <- function(n = nrow(dat), data = dat, ratio = 2/3, lags = NULL, unwante
   
   temp_data <-  as.data.frame(scale(temp_data, center = min, scale = max - min))
   
+  
+  # Change levels of default data from 0 and 1 to non-default and default. Needed for input in care functions
+  temp_data$default_time <-  as.factor(ifelse(temp_data$default_time==0, 
+                                              "non-default", 
+                                              "default" ))#, unique = T)
+  
+  levels(temp_data$default_time) <- make.names(levels(factor(temp_data$default_time)))
+  
   # Select ratio of the data as trainign data
   index <- sample(1:nrow(temp_data), (nrow(temp_data)*ratio) %>% ceiling)
+  
   
   # Assign training data in global environment
   train_data <<- temp_data[index,]
@@ -105,23 +112,6 @@ shuffle <- function(n = nrow(dat), data = dat, ratio = 2/3, lags = NULL, unwante
   # Assign testing data in global environment
   test_data <<- temp_data[-index, ]
   
-  
-}
-
-# Function to create prediction evaluation matrix
-prediction_matrix <- function(predictions, observations = test_data$default_time, tr = 0.5){
-  
-  #transform to binary response
-  predictions <- ifelse(predictions < tr, 0,1)
-  observations <- ifelse(observations < tr, 0,1)
-  
-  #get accordances
-  nn <- which(predictions == 0) %in% which(observations == 0) %>% sum
-  oo <- which(predictions == 1) %in% which(observations == 1) %>% sum
-  no <- which(predictions == 0) %in% which(observations == 1) %>% sum
-  on <- which(predictions == 1) %in% which(observations == 0) %>% sum
-  
-  matrix(c(oo,on,no,nn), nrow = 2, byrow = T) %>% set_rownames(1:2) %>% set_colnames(1:2)
   
 }
 
@@ -140,11 +130,11 @@ evaluate_model <- function(model=list(...), modelname, observations = test_data$
       predictions <- predict(model, newx = test_data[ ,-17] %>% as.matrix, s = best_lamb, type = "response")
       
     }else{
-      predictions <- as.integer(predict(model[[i]], newdata=test_data))-1#, type = "response")
+      predictions <- predict(model[[i]], newdata=test_data)#, type = "response")
     }
-    predictions_bin <- ifelse(predictions < tr, 0,1)
+    #predictions_bin <- ifelse(predictions < tr, 0,1)
     
-    table  <- prediction_matrix(predictions)
+    table  <- confusionMatrix(predictions, test_data$default_time)$table
     # table  <- table(observations, predictions_bin, dnn = c("Actual defaults", "Predicted defaults"))[c(2,1),c(2,1)] # Create matrix and Change order columns of matrix
     
     TRP    <- table[1,1]/(table[1,1]+table[1,2])   # True Positive Rate (TPR) or sensitivity or recall or hit rate is a measure of how
@@ -158,9 +148,9 @@ evaluate_model <- function(model=list(...), modelname, observations = test_data$
     # the predicted ones are actually correctly predicted. If precision is closer to one, we are more accurate in our predictions
     Recall <- table[1,1]/(table[1,1]+table[1,2])   # Recall, on the other hand, tells how many relevant items we selected.
     
-    mse_cont    <- (predictions-observations) ^2 %>% mean
-    mse_bin     <- (predictions-observations) ^2 %>% mean
-    metrics[[i]]<- data.frame(Model = modelname[i], TRP = TRP, TNR=TNR, ACC=ACC, Precision=Prec, Recall=Recall, MSE_cont = mse_cont, MSE_bin=mse_bin)
+    #mse_cont    <- (predictions-observations) ^2 %>% mean
+    #mse_bin     <- (predictions-observations) ^2 %>% mean
+    metrics[[i]]<- data.frame(Model = modelname[i], TRP = TRP, TNR=TNR, ACC=ACC, Precision=Prec, Recall=Recall) #MSE_cont = mse_cont, MSE_bin=mse_bin)
     tables[[i]] <- table
   }
   
@@ -170,6 +160,8 @@ evaluate_model <- function(model=list(...), modelname, observations = test_data$
   list(tables, metrics_all)
   #return(list(matrix = mat, detected_defaults = detected_defaults, mse_cont = mse_cont, mse_bin = mse_bin))
 }
+
+
 
 # Feed with input from evaluate_model
 plot_evaluation <- function(evaluate_model_object){ # Insert valuation metrics
